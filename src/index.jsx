733a1eb41320cf53530f6114d2aaab8bc163be26
egild <loku56@mail.ru>
2018-03-14 08:15:39 +0500

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './assets/styles/index.scss';
+
+ReactDOM.render(
+    <div>
+        hello world
+    </div>,
+    document.getElementById('task-manager')
+);
---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const publicPath = '/dist/build/';
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: './src/index.jsx',
+    devtool: 'cheap-module-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Task manager',
+            template: './index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+
+    output: {
+        path: path.join(__dirname, publicPath),
+        filename: '[name].bundle.js',
+        publicPath: publicPath,
+        sourceMapFilename: '[name].map',
+    },
+
+    devServer: {
+        port: 3000,
+        host: 'localhost',
+        historyApiFallback: true,
+        noInfo: false,
+        stats: 'minimal',
+        publicPath: publicPath,
+        contentBase: path.join(__dirname, publicPath),
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, {
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif|ico)$/,
+                exclude: [/fonts/],
+                loader: 'file-loader?name=images/[name].[ext]'
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf|svg)(\?\S*)?$/,
+                exclude: [/images/],
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.js|.jsx?$/,
+                exclude: /(node_modules)/,
+                loaders: ["babel-loader"]
+            }
+        ],
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            }
+        ]
+    },
+}
